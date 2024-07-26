f2541d8118c8b7ebf1f4ae0d370283aba60acb73
NINE-J <rnwo47@gmail.com>
2024-07-26 15:36:12 +0900

    [A] Edit Channel Modal
    - delete channel
    - edit channel

---
+++ components/modals/delete-channel-modal.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import qs from "query-string";
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog"
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+
+export const DeleteChannelModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "deleteChannel";
+  const { server, channel } = data;
+  
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const url = qs.stringifyUrl({
+        url: `/api/channels/${channel?.id}`,
+        query: {
+          serverId: server?.id,
+        }
+      })
+      
+      await axios.delete(url);
+
+      onClose();
+      router.push(`/servers/${server?.id}`);
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            채널 삭제
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            정말 채널을 삭제하시겠습니까?<br />
+            <span className="text-indigo-500 font-semibold">#{channel?.name}</span> 영구적으로 삭제됩니다.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isLoading}
+              onClick={onClose}
+              variant="ghost"
+            >
+              취소
+            </Button>
+            <Button
+              disabled={isLoading}
+              onClick={onClick}
+              variant="primary"
+            >
+              확인
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
