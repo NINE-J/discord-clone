7b9a221ddd7cb80f5a324ed960b6a945fc0ab550
NINE-J <rnwo47@gmail.com>
2024-08-14 13:13:40 +0900

    [M] Fix socket

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -13,9 +13,11 @@ export const config = {
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if(!res.socket.server.io) {
     const path = "/api/socket/io";
+
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIo(httpServer, {
       path: path,
+      transports: ['websocket'],
       addTrailingSlash: false,
     });
     res.socket.server.io = io;
